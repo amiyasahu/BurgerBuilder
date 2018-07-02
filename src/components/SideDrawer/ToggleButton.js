07b6f0fcf98306349a47de0c4ec515de36facceb
Amiya Sahu <amiya053@gmail.com>
2018-07-02 07:39:42 -0400

    Updated SVG

--- src/components/SideDrawer/ToggleButton.js
+++ src/components/SideDrawer/ToggleButton.js
@@ -5,7 +5,10 @@ const toggleButton = (props) => {
         <button className="Button" onClick={props.openDrawer}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30" focusable="false">
                 <title>Menu</title>
-                <path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10"
+                <path stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeMiterlimit="10"
                       d="M4 7h22M4 15h22M4 23h22"></path>
             </svg>
         </button>
