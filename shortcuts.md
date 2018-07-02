c581d6f86cecb3b56d09baea3181ffa306c08d4d
Amiya Sahu <amiya053@gmail.com>
2018-07-02 00:19:01 -0400

    Updated toggle button on toolbar

---
+++ shortcuts.md
@@ -0,0 +1,13 @@
+**rccp** Creates a React component class with PropTypes and ES6 module system
+
+rcfc Creates a React component class with PropTypes and all lifecycle methods and ES6 module system
+
+**ren** render with all properties
+
+rrc Creates a React component class connected to redux
+
+**rsc** Creates a stateless React component without PropTypes and ES6 module system
+
+rscp Creates a stateless React component with PropTypes and ES6 module system
+
+ssf Performs a shal low merge of nextState into current state
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Logo } from "../index";
 import Navigation from '../Navigation/Navigation';
+import ToggleButton from '../SideDrawer/ToggleButton';
 import './Toolbar.css';
 
 const toolbar = (props) => {
@@ -17,15 +18,5 @@ const toolbar = (props) => {
     );
 };
 
-const ToggleButton = (props) => {
-    return (
-        <button className="Button" onClick={props.openDrawer}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30" focusable="false">
-                <title>Menu</title>
-                <path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10"
-                      d="M4 7h22M4 15h22M4 23h22"></path>
-            </svg>
-        </button>
-    );
-}
+
 export default toolbar;
